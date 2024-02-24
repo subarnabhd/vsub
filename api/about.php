227d33cca6d2330c617eeaea3c10cd0490cf50b2
Subarna  Bhandari <64469023+subarnabhd@users.noreply.github.com>
2024-02-24 13:03:57 +0545

    test

--- api/about.php
+++ api/about.php
@@ -39,9 +39,7 @@
 
 <body>
 
-  <header class="theader minimal">
-    <nav id="main-nav"></nav>
-  </header>
+<?php include 'header.php'; ?>
 
   <main>
 
@@ -97,78 +95,13 @@
     </div>
   </main>
 
-  <footer>
-    <div class="scopywrite"></div>
-  </footer>
 
-   <!-- Modal Menu Start-->
- <div class="menu">
-  <div class="modal fade " id="exampleModalmenu" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-fullscreen">
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <div class="left">
-            <ul>
-              <li><a href="/index.html" class="modal-dot">Home</a></li>
-              <li><a href="/about.html" class="modal-dot">About</a></li>
-              <li><a href="/studio.html" class="modal-dot">Studio<strong>.</strong></a></li>
-              <li><a href="/portfolio.html" class="modal-dot">Portfolio</a></li>
-              <li><a href="/contact.html" class="modal-dot">Contact</a></li>
-
-            </ul>
-          </div>
-          <div class="center">
-            <ul>
-              <h3>Useful Link</h3>
-              <li><a href="/blog.html" class="modal-sm-dot">Good Reads</a></li>
-              <li><a target="_blank" href="https://read.cv/subarnabhd" class="modal-sm-dot">Resume</a></li>
-              <li><a href="#" class="modal-sm-dot">Store</a></li>
-
-              <li><a href="/link.html" class="modal-sm-dot">Link</a></li>
-              <li><a href="/tools.html" class="modal-sm-dot">Tools</a></li>
-              <li><a target="_blank" href="http://pay.subarnabhandari.com/" class="modal-sm-dot">Pay</a>
-              </li>
-
-            </ul>
-          </div>
-          <div class="right">
-            <h3>Got an idea?</h3>
-            <h2>Together, let‘s create
-              something wonderful.</h2>
-            <a href="/contact.html" class="btn" type="button">Start your project</a>
-          </div>
-        </div>
-        <div class="modal-footer">
-          <div class="modal-container">
-            <div class="left">
-              <li><a target="_blank" href="https://facebook.com/subarnabhd" class="m-foot-link">Facebook</a></li>
-              <li><a target="_blank" href="https://instagram.com/subarnabhd" class="m-foot-link">Instagram</a></li>
-              <li><a target="_blank" href="https://www.linkedin.com/in/subarnabhd" class="m-foot-link">Linkedin</a>
-              </li>
-              <li><a target="_blank" href="https://twitter.com/subarnabhd" class="m-foot-link">Twitter</a></li>
-              <li><a target="_blank" href="https://behance.net/subarnabhd" class="m-foot-link">Behance</a></li>
-              <li><a target="_blank" href="https://dribbble.com/subarnabhd" class="m-foot-link">Dribbble</a></li>
-              <li><a target="_blank" href="/link.html" class="m-foot-link">More..</a></li>
-
-            </div>
-            <div class="right">
-              <li><a target="_blank" href="mailto:hello@subarnabhandari.com">hello@subarnabhandari.com</a></li>
-
-            </div>
+  <?php include 'copywrite.php'; ?>
+  <?php include 'modal.php'; ?>
 
 
 
-          </div>
-        </div>
 
-      </div>
-    </div>
-  </div>
-</div>
-<!-- Modal Menu End-->
 
 
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
